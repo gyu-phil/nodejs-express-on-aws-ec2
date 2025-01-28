6b6b2bdc6ed6fc8e33f3cec50f304469f228d328
worldvit <ccna.kim@gmail.com>
2025-01-28 22:02:45 +0900

    v3: update

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) =>{
-    res.send('<h1>Express Deom</h1><h4>Message: Success</h4><p>Version 1.0<p>');
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-app.get('/products',(req,res) =>{
-    res.send([
-        {
-            productId: '101',
-            price: 100
-        },
-        {
-            productId: '102',
-            price: 150
-        }
-    ])
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
 })
 
-app.listen(port, ()=>{
-    console.log(`dome App is up and listening to port: ${port}`);
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
 })
+ 
