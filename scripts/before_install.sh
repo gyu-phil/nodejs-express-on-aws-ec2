8157a3bb880e66bec024cabd485bf23d790afae9
worldvit <ccna.kim@gmail.com>
2025-01-28 21:58:02 +0900

    v2: Update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash . ~/.nvm/nvm.sh
 nvm install node
 
 # create our working directory if it does't exist
